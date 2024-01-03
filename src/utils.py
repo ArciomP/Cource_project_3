1b6049841b204a3bc92daf75fc9f5f93cbfe226d
Arciom Pietroševič <temat31@mail.ru>
2024-01-03 21:35:17 +0300

    Create a function to get a list with everything you need

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
 import json
 
 def get_all_operations(path):
+    """
+    Функция забирает данные из json
+    """
     with open(path) as file:
         content = json.load(file)
     return content
+
+
+def get_executed_only(operation:list)->list:
+    """
+    Функция создает новый список без всего лишнего
+    """
+    operation_list = [stage for stage in operation if stage != {} and stage["state"] == "EXECUTE"]
+    return operation_list
+
+
